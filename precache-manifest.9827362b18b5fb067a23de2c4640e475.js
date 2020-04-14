6e13059219cffbb5b680e1eb963186c17b8c4697
kadirijhay <39244828+KadiriJhay@users.noreply.github.com>
2020-04-14 17:22:56 +0100

    Updates

---
+++ precache-manifest.9827362b18b5fb067a23de2c4640e475.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8662f0bdd7d5c19b66bf923fc0b2105a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "226a91035df342ee0ed8",
+    "url": "./static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "37b71e442e4229eb9b9e",
+    "url": "./static/css/main.28eefd57.chunk.css"
+  },
+  {
+    "revision": "226a91035df342ee0ed8",
+    "url": "./static/js/2.7ecf8f3f.chunk.js"
+  },
+  {
+    "revision": "37b71e442e4229eb9b9e",
+    "url": "./static/js/main.b5dd55bd.chunk.js"
+  },
+  {
+    "revision": "c16bf568d71155fcecfa",
+    "url": "./static/js/runtime-main.de270b5e.js"
+  }
+]);
